4e992b4cf491eca1d612d656a3eeeaa95d253d43
Davi Ferreira <contato@daviferreira.com>
2015-09-12 23:41:55 +0200

    Add share to demo

--- demo/index.jsx
+++ demo/index.jsx
@@ -25,6 +25,14 @@ class Demo extends React.Component {
         <div itemClass="has-overlay" itemStyle={{ backgroundImage: `url(${wallpaper})`, backgroundSize: 'cover' }}>
           <div className="content">Yo.</div>
         </div>
+        <div itemStyle={{ color: '#333' }}>
+          <div className="content love">
+            <i className="fa fa-heart"></i>
+            <iframe src="http://ghbtns.com/github-btn.html?user=daviferreira&repo=react-viewport-slider&type=follow&count=true&size=large" allowTransparency="true" frameBorder="0" scrolling="0" width="auto" height="30" />
+            <iframe src="http://ghbtns.com/github-btn.html?user=daviferreira&repo=react-viewport-slider&type=watch&count=true&size=large" allowTransparency="true" frameBorder="0" scrolling="0" width="auto" height="30" />
+            <iframe src="http://ghbtns.com/github-btn.html?user=daviferreira&repo=react-viewport-slider&type=fork&count=true&size=large" allowTransparency="true" frameBorder="0" scrolling="0" width="auto" height="30" />
+          </div>
+        </div>
       </Slider>
     );
   }
