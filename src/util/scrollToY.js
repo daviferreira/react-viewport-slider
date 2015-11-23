9748d74cdcabd0aed67d1b01a19f4b07a43b9e72
Davi Ferreira <contato@daviferreira.com>
2015-11-23 13:36:10 +0100

    lintz

--- src/util/scrollToY.js
+++ src/util/scrollToY.js
@@ -4,8 +4,8 @@ window.requestAnimFrame = (() => {
   return window.requestAnimationFrame     ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame  ||
-    function( callback ){
-    window.setTimeout(callback, 1000 / 60);
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
     };
 })();
 
